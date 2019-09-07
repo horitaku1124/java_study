3a5fa9df531ef9ff96811d30154df18646b7e615
Horimatsu Takuya <new_woory@hotmail.com>
2019-09-07 17:36:47 +0900

    add performance test

---
+++ src/main/java/util/ObjectArrayList.java
@@ -0,0 +1,67 @@
+package util;
+
+public class ObjectArrayList<R> {
+  private static final int DefaultCapacity = 5;
+  private Object[] internal;
+  private int size;
+  private int capacity;
+  public ObjectArrayList(int n) {
+    capacity = n;
+    internal = new Object[capacity];
+  }
+  public ObjectArrayList() {
+    this(DefaultCapacity);
+  }
+
+  /**
+   *
+   * <ul>
+   *     <li>100  :  560us</li>
+   *     <li>500  :   58us</li>
+   *     <li>1k   :   68us</li>
+   *     <li>5k   :  345us</li>
+   *     <li>10k  :  485us</li>
+   *     <li>50k  :  2.5ms</li>
+   *     <li>100k :  3.1ms</li>
+   * </ul>
+   * <p>O(N)</p>
+   *
+   * @param v value to store
+   */
+  public void add(R v) {
+    if (size == capacity) {
+      int cap = capacity * 2;
+      Object[] newInternal = new Object[cap];
+      if (size >= 0) {
+        System.arraycopy(internal, 0, newInternal, 0, size);
+      }
+      internal = newInternal;
+      capacity = cap;
+    }
+    internal[size++] = v;
+  }
+
+  public R get(int i) {
+    return (R) internal[i];
+  }
+  public int size() {
+    return size;
+  }
+  public void set(int i, R value) {
+    internal[i] = value;
+  }
+  public boolean contains(R key) {
+    for(int i = 0;i < size;i++) {
+      if (internal[i].equals(key)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void clear() {
+    size = 0;
+    capacity = DefaultCapacity;
+    internal = new Object[DefaultCapacity];
+  }
+}
---
+++ src/test/java/util/ist/ObjectArrayListTest.java
@@ -0,0 +1,44 @@
+package util.ist;
+
+import org.junit.Test;
+import util.ObjectArrayList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ObjectArrayListTest {
+    @Test
+    public void test1() {
+        ObjectArrayList<Integer> list = new ObjectArrayList<>();
+        assertThat(list.size(), is(0));
+        list.add(1);
+        assertThat(list.size(), is(1));
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        assertThat(list.size(), is(4));
+
+        assertThat(list.get(0), is(1));
+        assertThat(list.get(1), is(2));
+        assertThat(list.get(2), is(2));
+        assertThat(list.get(3), is(3));
+    }
+
+    @Test
+    public void add_performanceTest() {
+        int[] nums = {100, 500, 1_000, 5_000, 10_000, 50_000, 100_000};
+        for (int num: nums) {
+//            System.out.println("N=" + num);
+
+            long started = System.nanoTime();
+            ObjectArrayList<Integer> list = new ObjectArrayList<>();
+            for (int i = 0;i < num;i++) {
+                list.add(i);
+            }
+            long stopped = System.nanoTime();
+            double elapse = stopped - started;
+            System.out.format("N=%12d - %,15.0fns\n", num, elapse);
+            assertThat(list.size(), is(num));
+        }
+    }
+}
